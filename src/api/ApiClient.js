c15bb28b2a407507b92bbeac0a7c5df0ab4028c5
dawoodxv <muhammeddawood414@gmail.com>
2024-11-11 10:53:32 -0600

    Add more fucntions to api

--- src/api/ApiClient.js
+++ src/api/ApiClient.js
@@ -8,3 +8,13 @@ export const fetchProductsByType = (type) => {
   const allProducts = fetchProducts();
   return allProducts.filter((product) => product.type === type);
 };
+
+export const fetchProduct = (id) => {
+  const allProducts = fetchProducts();
+  return allProducts.find((product) => product.id === id) || null;
+};
+
+export const fetchBundle = (ids) => {
+  const allProducts = fetchProducts();
+  return allProducts.filter((product) => ids.includes(product.id));
+};
